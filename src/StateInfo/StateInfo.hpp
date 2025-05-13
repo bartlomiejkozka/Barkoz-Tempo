dcc360aad7b8fb3018cecaed6a1f84fa037de844
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-05-13 23:02:52 +0200

    feat: minot changes in state pos manag

--- src/StateInfo/StateInfo.hpp
+++ src/StateInfo/StateInfo.hpp
@@ -2,10 +2,11 @@
 #define STATE_INFO_HPP
 
 #include <cstdint>
+#include <stack>
+#include <stdexcept>
 
-// maybe type of el. in std::stack
-struct StateInfo
-{
+
+struct posInfo {
     // stockFish also use pawnKey hash, materialKey hash pliesFromNull
     uint64_t hash;
     uint64_t from;
@@ -16,5 +17,31 @@ struct StateInfo
     uint8_t capturedPiece;
 };
 
+
+class StateInfo
+{
+private:
+    std::stack<posInfo> stateStack;
+
+public:
+    //--------Contructors-----------
+    StateInfo() = default;
+    ~StateInfo() = default;
+    StateInfo(StateInfo &other) = default;
+    StateInfo& operator=(const StateInfo& other) = default;
+
+    //---------Methods---------------
+    void addPos(const posInfo &positionInfo) { stateStack.push(positionInfo); }
+    posInfo getPos() 
+    { 
+        if (!stateStack.empty())
+        { 
+            posInfo tempPos = stateStack.top();
+            stateStack.top(); 
+            return tempPos;
+        }
+        throw std::out_of_range("State Stack is empty");
+    }
+};
 
 #endif
