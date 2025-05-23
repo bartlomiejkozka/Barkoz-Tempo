1220341e5780efd8b823a6c5f1b003db07448de2
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-05-23 16:24:07 +0200

    minor change

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main()
 {
     Board testBoard;
-    StateInfo testStateInfo;
+    StateInfo_t testStateInfo;
     std::array<uint64_t, Board::boardSize> bitBoards = {};
     PieceMap pieceMap;
     uint64_t hash = pieceMap.generatePosHash(bitBoards);
