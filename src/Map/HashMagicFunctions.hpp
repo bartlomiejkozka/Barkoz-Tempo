491cc078adaf9aae1b8ca5e3fa0609caafb45ffc
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-06-06 18:54:14 +0200

    minor change

--- src/Map/HashMagicFunctions.hpp
+++ src/Map/HashMagicFunctions.hpp
@@ -4,15 +4,44 @@
 #include <cstdint>
 #include <chrono>
 #include <random>
+#include <utility>
 
+
+/*
+* The Class concept is dedicated to
+* generate random unique index for magic boards by brude force method - "Trial and Error"
+* hold magic number and offset for specific bishop/rook position
+*/
 
 typename<class RandomGenerator = std::mt19937_64>
 class FancyMagicFunction {
+
+    // [magic number, offset]
+    using FancyPair std::pair<uint64_t, uint8_t>;
+
+    public:
+    
     //--------------------
     // Intilizators
     //--------------------
-        
+    
+    // Main usage to find the magic
+    explicit FancyMagicFunction(const uint8_t offset) {  }
+    
+    constexpr FancyMagicFunction(const FancyPair &params):
+        : _magic(magic), _offset(offset) {}
 
+    // Probably unused
+    constexpr FancyMagicFunction(const uint64_t magic, const uint8_t offset)
+        : _magic(magic), _offset(offset) {}
+
+
+
+
+
+    private:
+    uint64_t _magic;
+    uint8_t _offset;
 };
 
 
