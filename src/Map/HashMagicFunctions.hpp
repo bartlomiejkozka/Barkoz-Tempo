8f36d484591c5e55d18c18245f4b49993cb44d71
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-06-06 18:17:02 +0200

    Add Hash Magic Functions

---
+++ src/Map/HashMagicFunctions.hpp
@@ -0,0 +1,19 @@
+#ifndef HASH_MAGIC_FUNCTIONS_HPP
+#define HASH_MAGIC_FUNCTIONS_HPP
+
+#include <cstdint>
+#include <chrono>
+#include <random>
+
+
+typename<class RandomGenerator = std::mt19937_64>
+class FancyMagicFunction {
+    //--------------------
+    // Intilizators
+    //--------------------
+        
+
+};
+
+
+#endif
