eb04feb75bbfd52396a2257a635f617c2c52cc16
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-10-03 23:07:49 +0200

    feat: Add Rook, Bishop, Queen MoveGen.

---
+++ src/MoveGenerator/QueenMap.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2025 Bartlomiej Kozka
+// All rights reserved.
+
+/*************** File description ****************/
+/* Queen moves Map using magics                  */
+/*************************************************/
+
+#ifndef QUEEN_MAP
+#define QUEEN_MAP
+
+#include <cstdint>
+#include <array>
+
+class Queen 
+{
+    public:
+    //--------------------
+    // Initilizers
+    //--------------------
+    
+    Queen() = delete;
+    
+    //------------------
+    // Main API function
+    //------------------
+
+    [[nodiscard("PURE FUN")]] static constexpr uint64_t getMoves()
+    {
+        
+    }
+
+    private:
+
+};
+
+#endif // QUEEN_MAP
