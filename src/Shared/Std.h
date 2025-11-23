bb3d397da2c6a20015de701407ff1bfd3bf43c32
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-11-23 12:54:01 +0100

    m.fix

--- src/Shared/Std.h
+++ src/Shared/Std.h
@@ -5,9 +5,9 @@
 
 #if USING_GCC
     #include <cstdlib>
-    INLINE int ABS (int x) { return std::abs(x); }
+    constexpr int ABS (int x) { return std::abs(x); }
 #else
-    INLINE constexpr int ABS(int x) { return x < 0 ? -x : x; }
+    constexpr int ABS(int x) { return x < 0 ? -x : x; }
 #endif
 
 
