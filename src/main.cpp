5b61ab11dc2a8dbcc2ea17c3c0cd082b708295d1
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-11-26 21:54:57 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -22,24 +22,35 @@ int main()
 
     board.init();
 
-    std::string m  = "d2d3";
-    std::string m1 = "c7c6"; 
-    std::string m2 = "e1d2";
-    std::string m3 = "d8a5";
+    board.loadFromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0");
+
+    std::string m  = "e1f1";
+    std::string m1 = "b8c6"; 
+    std::string m2 = "e2e3";
+    std::string m3 = "c6d4";
+    std::string m4 = "f1c4";
+    std::string m5 = "g7g5";
+
     auto fromTo = std::make_pair(static_cast<int>(SimpleParser::parseMoveString(m).from), static_cast<int>(SimpleParser::parseMoveString(m).to));
     Move mv(fromTo.first, fromTo.second, 0);
-    rules._board.makeMove(mv);
+    // rules._board.makeMove(mv);
     fromTo = std::make_pair(static_cast<int>(SimpleParser::parseMoveString(m1).from), static_cast<int>(SimpleParser::parseMoveString(m1).to));
     Move mv1(fromTo.first, fromTo.second, 0);
-    rules._board.makeMove(mv1);
+    // rules._board.makeMove(mv1);
     fromTo = std::make_pair(static_cast<int>(SimpleParser::parseMoveString(m2).from), static_cast<int>(SimpleParser::parseMoveString(m2).to));
     Move mv2(fromTo.first, fromTo.second, 0);
-    rules._board.makeMove(mv2);
+    // rules._board.makeMove(mv2);
     fromTo = std::make_pair(static_cast<int>(SimpleParser::parseMoveString(m3).from), static_cast<int>(SimpleParser::parseMoveString(m3).to));
-    Move mv3(fromTo.first, fromTo.second, 0);
-    rules._board.makeMove(mv3);
+    // Move mv3(fromTo.first, fromTo.second, 1);
+    // rules._board.makeMove(mv3);
+    fromTo = std::make_pair(static_cast<int>(SimpleParser::parseMoveString(m4).from), static_cast<int>(SimpleParser::parseMoveString(m4).to));
+    Move mv4(fromTo.first, fromTo.second, 0);
+    // rules._board.makeMove(mv4);
+    fromTo = std::make_pair(static_cast<int>(SimpleParser::parseMoveString(m5).from), static_cast<int>(SimpleParser::parseMoveString(m5).to));
+    Move mv5(fromTo.first, fromTo.second, 0);
+    // rules._board.makeMove(mv5);
 
-    uint64_t nodes = PerftDivide(1, rules, perft_stats);
+    uint64_t nodes = PerftDivide(2, rules, perft_stats);
 
     perft_stats.print();
 
@@ -47,3 +58,5 @@ int main()
 
     return 0;
 }
+
+// 24, 25
